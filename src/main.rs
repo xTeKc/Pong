a4b4eab030297cc8bd80be8dcc8e5c20125b4508
xTeKc <bcd3v@pm.me>
2021-08-16 23:17:09 -0400

    comment out for now

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     App::build().run();
 }
 
-struct Position { x: f32, y: f32}
+// struct Position { x: f32, y: f32}
 
-fn print_position_system(query: Query<&Transform>) {
-    for transform in query.iter() {
-        println!("position: {:?}", transform.translation);
-    }
-}
+// fn print_position_system(query: Query<&Transform>) {
+//     for transform in query.iter() {
+//         println!("position: {:?}", transform.translation);
+//     }
+// }
 
-struct Entity(u64);
+// struct Entity(u64);
