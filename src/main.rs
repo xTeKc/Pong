62b514fc41c911c9b22b555e173fd6f66499f922
xTeKc <bcd3v@pm.me>
2021-08-16 23:19:17 -0400

    setup func to run

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::*;
 
+fn hello_world() {
+    println!("Hello World");
+}
+
 fn main() {
-    App::build().run();
+    App::build()
+        .add_system(hello_world.system())
+        .run();
 }
 
 // struct Position { x: f32, y: f32}
