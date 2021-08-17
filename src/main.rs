30c15477f63123f694ee7acf1433400dd26dd042
xTeKc <bcd3v@pm.me>
2021-08-16 23:34:41 -0400

    add startup sys

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,29 @@ fn hello_world() {
 
 fn main() {
     App::build()
+        //registers the startup system
+        .add_startup_system(add_people.system())
         //converts hello_world func into system type
         //add_system func adds system to apps schedule
         .add_system(hello_world.system())
         .run();
 }
+
+//
+struct Person;
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("John
+Doe".to_string()));
+}
+
+
+
+
+
+
+
+
 
 // struct Position { x: f32, y: f32}
 
