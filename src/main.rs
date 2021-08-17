ac8fbf283b94f62c305709f864ab5d910c8f0495
xTeKc <bcd3v@pm.me>
2021-08-16 23:14:38 -0400

    add some structs and func

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,13 @@ use bevy::prelude::*;
 fn main() {
     App::build().run();
 }
+
+struct Position { x: f32, y: f32}
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+struct Entity {u64};
