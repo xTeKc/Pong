9f9de9ffbec2f1d37af2fd7e513bef692bc87867
xTeKc <bcd3v@pm.me>
2021-09-26 18:36:41 -0400

    update imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process;
 use piston::window::WindowSettings;
 use piston::event_loop::{EventSettings, Events};
-use piston::{Button, Key, PressEvent, ReleaseEvent,
+use piston::input::{Button, Key, PressEvent, ReleaseEvent,
 RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use glutin_window::GlutinWindow;
 use opengl_graphics::{Glgraphics, OpenGL};
