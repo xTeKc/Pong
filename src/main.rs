c08ed9b4d0e30c71db39c81447051ff1ff673aba
xTeKc <bcd3v@pm.me>
2021-09-26 18:40:14 -0400

    create App struct and add data

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use glutin_window::GlutinWindow;
 use opengl_graphics::{Glgraphics, OpenGL};
 
+
+pub struct App {
+    gl: GlGraphics,
+    left_score: i32,
+    left_position: i32,
+    left_velocity: i32,
+    right_score: i32,
+    right_position: i32,
+    right_velocity: i32,
+    ball_x: i32,
+    ball_y: i32,
+    velocity_x: i32,
+    velocity_y: i32,
+}
+
+
 fn main() {
     
 }
